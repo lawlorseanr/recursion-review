a014a52973f04296db1ac53af1d4df16e4b03f6e
Sean Lawlor <lawlorseanr@gmail.com>
2021-06-14 17:47:39 -0700

    Setup type checks for obj stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var stringOutput = '';
+
+  // primitives
+  if (typeof obj === 'string') {
+    stringOutput += obj;
+  }
+
+  if (typeof obj === 'number') {
+    stringOutput += obj.toString();
+  }
+
+  if (typeof obj === 'boolean') {
+    stringOutput += obj.toString();
+  }
+
+  if (obj === null) {
+    stringOutput += 'null';
+  }
+
+  if (obj === undefined || typeof obj === 'function') {
+
+  }
+
+  // array
+  if (Array.isArray(obj)) {
+
+  }
+
+  // object
+  if (typeof obj === 'object') {
+
+  }
+
+  return stringOutput;
+
 };
